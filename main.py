8a9d420dc2c4a930161862e8fa08655ea52ea8be
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-04-28 15:33:47 +0800

    creating gui with kivy user auth

---
+++ main.py
@@ -0,0 +1,25 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+Builder.load_file('design.kv')
+
+#create classes to match design.kv
+class LoginScreen(Screen):
+    def sign_up(self):
+        self.manager.current = "sign_up_screen"
+
+class RootWidget(ScreenManager):
+    pass
+
+class SignUpScreen(Screen):
+    pass
+
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+if __name__ == "__main__":
+    MainApp().run()
