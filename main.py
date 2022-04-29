7933945fa6093fd9410d02f4de73ba6f56aec5b9
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-04-29 09:07:11 +0800

    adding new signupsuccess screen

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+from datetime import datetime
 
 
-Builder.load_file('design.kv')
+
+Builder.load_file("design.kv")
 
 #create classes to match design.kv
 class LoginScreen(Screen):
@@ -13,8 +16,25 @@ class LoginScreen(Screen):
 class RootWidget(ScreenManager):
     pass
 
+
+
 class SignUpScreen(Screen):
+    def add_user(self, uname, pword): 
+        with open("users.json") as file:
+            users = json.load(file)
+        print(users)
+
+        #to allow us create another pair of values
+        users[uname] = {'username': uname, 'password': pword,
+            'created' : datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+
+        with open("users.json", 'w') as file:
+            json.dump(users, file)
+        self.manager.current = "sign_up_screen_success"
+
+class SignUpScreenSuccess(Screen):
     pass
+
 
 
 class MainApp(App):
